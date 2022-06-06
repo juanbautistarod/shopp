d5640f0068c983b077aba02d59d21e14a5509829
Ezequiel Braunstein <ezequiel.braunstein@gmail.com>
2022-06-06 15:18:13 -0300

    (bug): Resuelvo bug back button home page + Unit test

--- tests/unit/ui/pagination.spec.js
+++ tests/unit/ui/pagination.spec.js
@@ -4,7 +4,7 @@
 
 const utils = require('../../ui-utils.js')
 
-const { getByText } = require('@testing-library/dom');
+const { getByText, TestingLibraryElementError } = require('@testing-library/dom');
 require('@testing-library/jest-dom');
 
 function renderPagination(pagination) {
@@ -85,4 +85,19 @@ describe('Paginación', () => {
         expect(prevLink).toBeVisible();
         expect(prevLink.getAttribute('href')).toBe(`?page=${pagination.currentPage - 1}`);
     });
+
+    test('Deberia no mostrase el boton anterior en la primera pagina', async () => {
+        const pagination = {
+            totalPages: 10,
+            currentPage: 1
+        }
+        const html = renderPagination(pagination);
+
+        document.body.innerHTML = html;
+
+
+        expect(() => {
+            getByText(document.body, '< Anterior');
+        }).toThrowError(TestingLibraryElementError);
+    })
 });
